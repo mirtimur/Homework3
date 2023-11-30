91dfb1eb60e9bf90fd1e3d949816edca7e6b8ac5
Mir TImur <mirtimur1@gmail.com>
2023-11-30 14:57:54 +0300

    Homework

--- TMSHomework3Mir/TMSHomework3Mir/ViewController.swift
+++ TMSHomework3Mir/TMSHomework3Mir/ViewController.swift
@@ -1,12 +1,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        testFunctions()
     }
-
-
+    
+    func testFunctions() {
+        print(sumOfFourDigits(1234))
+        
+        universal(10, 2)
+        universal(10.0, 2.0)
+        universal(2, 3, 5)
+        universal(15, 3.0)
+        stringComparison("авб", "ввш")
+        degreeExpansion(2, 4)
+    }
+    
+    // MARK: - Универсальные функции
+    func universal(_ a: Int, _ b: Int) {
+        print (a / b)
+    }
+    
+    func universal(_ a: Double, _ b: Double ) {
+        print (a * b)
+    }
+    
+    func universal(_ a: Int, _ b: Int, _ c: Int) {
+        print (a + b + c)
+    }
+    
+    func universal(_ a: Int, _ b: Double) {
+        print(a - Int(b))
+    }
+    
+    //MARK: - Сложение четырехзначного числа
+    func sumOfFourDigits(_ a: Int) -> Int {
+        var sum = 0
+        var a = a // ??? Why??
+        
+        while a > 0 {
+            sum += a % 10
+            a /= 10
+        }
+        return sum
+    }
+    
+    //MARK: - Сравнение строк
+    func stringComparison(_ a: String, _ b: String) {
+        if a > b {
+            print("Первая строка больше")
+        } else {
+            print("Вторая строка больше")
+        }
+    }
+    
+    //MARK: - Циклический вызов функции
+    
+    func cyclic(_ a: Int) {
+        print(a)
+        cyclic(2)
+    }
+    
+    //MARK: - Функция возведения в степень
+    func degreeExpansion(_ a: Decimal, _ b: Int) {
+        let pow = pow(a, b)
+        print(pow)
+    }
 }
-
